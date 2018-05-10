5535311ff8e4b233a8f815d1ed73a474406ceeee
unknown <rafaelmarinids@gmail.com>
2018-05-10 08:42:31 -0300

    Primeiro commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        publicPath: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, exclude: /(node_modules|bower_components)/, loader: 'babel-loader', options: { presets: ['env'] } }, 
+            /*{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },*/
+            { test: /\.scss$/, use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS
+            ] },
+            { test: /\.(png|svg|jpg|gif)$/, use: [ 'file-loader' ] },
+            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: [ 'file-loader' ] },
+        ]
+    },
+    plugins: [
+        /*new HtmlWebpackPlugin({ // Gera o arquivo HTML de saída
+            title: 'Desafio iClinic',
+            favicon: './public/favicon.ico',
+            template: './public/index.hbs'
+        }),*/
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: { 
+        extensions: ['*', '.js', '.jsx'] 
+    },
+    devServer: {
+        publicPath: '/dist',
+        contentBase: path.resolve(__dirname, 'public'),
+        hot: true,
+        port: 3000
+    }
+};
